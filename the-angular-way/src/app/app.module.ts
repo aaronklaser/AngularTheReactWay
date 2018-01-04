0dabe3d3067709ee70d77be63700b8f175b1a92c
Klaser, Aaron <Aaron.Klaser@gmail.com>
2018-01-04 11:39:33 -0600

    Working Angular 4 ToDo

---
+++ the-angular-way/src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { TodoDataService } from './todo-data.service';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
