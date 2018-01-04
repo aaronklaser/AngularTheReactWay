67135189821f65ff64ff5d0a01e30da7826bfbeb
Klaser, Aaron <Aaron.Klaser@gmail.com>
2018-01-04 17:01:24 -0600

    Added all the page components that we are doing to need

--- angular-the-react-way/src/app/app.module.ts
+++ angular-the-react-way/src/app/app.module.ts
@@ -4,11 +4,25 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { AddTodoComponent } from './containers/add-todo/add-todo.component';
+import { VisibleTodoListComponent } from './containers/visible-todo-list/visible-todo-list.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { LinkComponent } from './components/link/link.component';
+import { TodoComponent } from './components/todo/todo.component';
+import { TodoListComponent } from './components/todo-list/todo-list.component';
+import { FilterLinkComponent } from './containers/filter-link/filter-link.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddTodoComponent,
+    VisibleTodoListComponent,
+    FooterComponent,
+    LinkComponent,
+    TodoComponent,
+    TodoListComponent,
+    FilterLinkComponent
   ],
   imports: [
     BrowserModule,
---
+++ angular-the-react-way/src/app/containers/visible-todo-list/visible-todo-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-visible-todo-list',
+  templateUrl: './visible-todo-list.component.html',
+  styleUrls: ['./visible-todo-list.component.sass']
+})
+export class VisibleTodoListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
